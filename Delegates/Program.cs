a791600f59fc7aafdd86909af7e941c35bd97b65
Че <keramzit3000@gmail.com>
2022-08-12 14:30:03 +0300

    Add project files.

---
+++ Delegates/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Delegates
+{
+    public class Program
+    {       
+        class Solution
+        {
+            public static void Main(string[] args)
+            {
+                
+            }
+        }
+    }    
+}
---
+++ Delegates/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    internal class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirthday { get; set; }
+        public int PhoneNumber { get; set; }
+
+        public User(string firstName, string lastName, DateTime dateOfBirthday, int phoneNumber)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            DateOfBirthday = dateOfBirthday;
+            PhoneNumber = phoneNumber;
+        }
+    }
+}
